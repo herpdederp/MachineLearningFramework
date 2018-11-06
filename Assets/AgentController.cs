d86a5566a45b5bbd0e736587388b429d33f8c661
Stanchion <callumgolds@hotmail.com>
2018-11-05 23:01:17 -0800

    init

---
+++ Assets/AgentController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+
+    bool right;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("SwapDirection", 3.0f);
+    }
+
+    void SwapDirection()
+    {
+        right = !right;
+
+        Invoke("SwapDirection", 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (right)
+            transform.Translate(Vector3.left * Time.deltaTime * 5f);
+        else transform.Translate(Vector3.right * Time.deltaTime * 5f);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    int numberOfHits;
+
+
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    int GetNumberOfHits()
+    {
+        return numberOfHits;
+    }
+
+    Vector3 GetDirectionOfNearestTarget()
+    {
+        return (transform.position - target.position);
+    }
+
+    void ShootInDirection(Vector3 direction)
+    {
+
+    }
+
+
+
+
+}
